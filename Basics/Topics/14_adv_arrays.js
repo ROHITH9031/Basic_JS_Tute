2bbd3625538561126cc881630e74532d5cda3be3
AtyamRohith <atyamrohith2005@gmail.com>
2026-07-06 19:31:17 +0530

    added advance array topics

---
+++ Basics/Topics/14_adv_arrays.js
@@ -0,0 +1,52 @@
+// ADVANCED ARRAY METHODS & TECHNIQUES
+
+// syntax
+
+// array.reduce(callback(accumulator, currentValue
+
+//             ) => [initialValue])
+
+// const numbers = [10,20,30];
+// const sum = numbers.reduce((accumulator,currentValue) =>{
+//     return accumulator + currentValue;
+// }, 0)
+// console.log(sum);
+
+// let sum1 = 0;
+// for(let i = 0; i<numbers.length; i++){
+//     sum1 = sum1 + numbers[i];
+// }
+// console.log(sum1);
+
+// // slice
+// const fruits = ["apple","banana","kiwi","mango"];
+// const slicedFruits = fruits.slice(1,3)
+// console.log(slicedFruits);
+// console.log(fruits);
+
+// splice
+// const splicedFruits = fruits.splice(1,3);
+// console.log(splicedFruits);
+// console.log(fruits);
+
+// CONCAT
+
+// let a1 = [1,2,3];
+// let a2 = [4,5,6];
+// console.log(a1.concat(a2));
+
+// let nestedArray = [1,2,[3,4],[5,6,7],8];
+// const flatendArray = nestedArray.flat();
+// console.log(flatendArray);
+
+// spreadarray ...
+// let a1 = [1,2,3];
+// let a2 = [4,5,6];
+
+// console.log(...a1,10,20,30,...a2);
+
+
+
+
+
+

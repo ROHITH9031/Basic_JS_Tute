43479c0380106e10a2e2e32e4ed4470d8d6ac566
AtyamRohith <atyamrohith2005@gmail.com>
2026-07-08 21:09:40 +0530

    added closure and lexical scope concepts

--- Intermediate/17_closures.js
+++ Intermediate/17_closures.js
@@ -12,3 +12,20 @@ function outer() {
 
 }
 outer();
+
+// CLOSURE
+// A closure is created when:
+
+// A function is inside another function.
+// The inner function uses variables from the outer function.
+// The inner function continues to access those variables even after the outer function has finished executing.
+function greet(name) {
+    return function msg(message) {
+        console.log(message,name);
+        
+        
+    };
+    
+}
+const greetRohith = greet("rohith");
+greetRohith("Hello");

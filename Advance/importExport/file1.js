2602dfb604dd7229e7834059ff4c1b7cbd913c61
AtyamRohith <atyamrohith2005@gmail.com>
2026-07-18 12:44:32 +0530

    added import and export topic

---
+++ Advance/importExport/file1.js
@@ -0,0 +1,13 @@
+// EXPORT FROM MODULES
+// NAMED EXPORT
+// export const name = "atyam rohith";
+// export function add(a,b) {
+//     return a + b;
+    
+// }
+
+// EXPORT DEFAULT / DEFAULT EXPORT => IN  ONE FILE WE CAN ABLE TO USE ONLY 1  DEFAULT EXPORT
+export default function add(a,b) {
+    return a + b;
+    
+}
---
+++ Advance/importExport/file2.js
@@ -0,0 +1,8 @@
+// IMPORT
+
+// import {name,add} from './file1.js';
+// console.log(name);
+// console.log(add(10,10));
+
+import add from './file1.js';
+console.log(add(90,10));
